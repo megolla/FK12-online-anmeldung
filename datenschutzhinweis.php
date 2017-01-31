d60522c9ae1394386eb4c74351650261f0fe3599
Matthias <meg@wachenfeld-golla.de>
2017-01-31 14:54:02 +0100

    Datenschutz-Seite und versch. HTML-Mail-Versuche
    Signed-off-by: Matthias <meg@wachenfeld-golla.de>

---
+++ datenschutzhinweis.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title>Fakultät für Design, Hochschule München, Online-Anmeldung Bachelor, Hinweis zum Datenschutz</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="">
+<meta name="author" content="Matthias Edler-Golla, 10/12/2016">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700" rel="stylesheet">
+<link rel="stylesheet" href="assets/style.css?v=1.0">
+</head>
+
+<body class="datenschutz">
+
+<main>
+	
+<header class="cf">
+<img id="logo" src="p/FK12_Logo.jpg" alt="Fakultät für Design, Hochschule München" srcset="p/FK12_Logo.jpg 600w, p/FK12_Logo_pur.jpg 317w" sizes="33vw">
+<h1>Fakultät für Design</h1>
+</header>
+
+<h2>Hinweis zum Datenschutz</h2>
+
+
+
+<h3>Wir freuen uns über Ihr Interesse an der Hochschule München.</h3>
+
+<p>Für uns ist der vertrauensvolle Umgang mit personenbezogenen Daten von Bewerberinnen und Bewerbern sehr wichtig.</p>
+
+<p>Mit der Übersendung des ausgefüllten Online-Bewerbungsformulars erklären Sie sich damit einverstanden, dass Ihre Daten gemäß gesetzlicher Bestimmungen gespeichert werden dürfen.</p>
+
+<p>Im Folgenden erläutern wir, welche Informationen wir während Ihres Bewerbungsprozesses erfassen und wie diese genutzt werden.</p>
+
+<h3>1. Personenbezogene Daten</h3>
+<p>Personenbezogene Daten sind Einzelangaben über persönliche oder sachliche Verhältnisse einer bestimmten oder bestimmbaren natürlichen Person. Darunter fallen Informationen wie z. B. Ihr Name, Ihre Anschrift, Ihre Telefonnummer oder Ihr Geburtsdatum. Darunter fallen auch personenbezogene Unterlagen, die sie in das Portal einstellen.</p>
+
+<h3>2. Erhebung und Verarbeitung von personenbezogenen Daten</h3>
+<p>Wenn Sie unsere Webseiten besuchen, speichern unsere Webserver standardmäßig zum Zweck der Systemsicherheit temporär die Verbindungsdaten des anfragenden Rechners, die Webseiten, die Sie bei uns besuchen, das Datum und die Dauer des Besuches, die Erkennungsdaten des verwendeten Browser- und Betriebssystem-Typs sowie die Webseite, von der aus Sie uns besuchen.</p>
+<p>Diese Daten werden nicht mit anderen Daten verbunden und nach maximal 7 Tagen gelöscht. Darüber hinausgehende personenbezogene Angaben wie Ihr Name, Ihre Anschrift, Telefonnummer oder Email-Adresse werden nicht erfasst, es sei denn, diese Angaben werden von Ihnen im Rahmen des Bewerbungsprozesses gemacht.</p>
+
+<h3>3. Einsatz von Cookies</h3>
+<p>Es werden sogenannte „Session Cookies“ im Portal eingesetzt. Diese werden automatisch nach Beenden der Session, d.h. dem Schließen des Browsers, gelöscht. Das Bewerberportal setzt beim Aufrufen der Bewerbungs-Formular-Seite einen Cookie in welchem die Referenz zur serverseitigen Session abgespeichert ist. Dieses Session Cookie wird verwendet um während des Bewerbungsvorganges alle Anfragen des Clients zu einem Bewerbungsvorgang zuzuordnen.</p>
+
+<h3>4. Nutzung und Weitergabe der personenbezogenen Daten</h3>
+<p>Die von Ihnen zur Verfügung gestellten personenbezogenen Daten verwenden wir ausschließlich zur Erfüllung Ihrer Wünsche und Anforderungen, also zur Bearbeitung Ihrer Bewerbung. Eine Weitergabe Ihrer personenbezogenen Daten erfolgt nur innerhalb der Hochschule München und nur im Rahmen des Bewerbungsprozesses an im Bewerbungsprozess beteiligte Personen. Eine Übermittlung von personenbezogenen Daten, d.h. eine Weitergabe an Stellen außerhalb der Hochschule München findet nicht statt.</p>
+
+<h3>5. Sicherheit</h3>
+<p>Alle notwendigen technischen und organisatorischen Sicherheitsmaßnahmen, um Ihre personenbezogenen Daten vor Verlust und Missbrauch zu schützen, werden von uns getroffen.</p>
+
+<p style="margin-top: 3em; padding-top: 1.5em; border-top: 1px solid #ccc; text-indent: 0;"><em>Aufnahme-Kommission der Fakultät für Design, München</em></p>
+
+
+</main>
+
+</body>
+</html>
